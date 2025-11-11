c49ac5a3ca3c0d94bb8af7065bb0579891469dae
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2025-11-11 01:37:54 -0500

    ali instead of kas for pngs

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -13,20 +13,21 @@ export default function About() {
 			</Head>
 
 			<Columns>
-				<TextBox big>
+				<TextBox big subtle>
 					<p><i>
-						Combining the music of America and Japan alike, alisongrace explores chiptune and video game music from a songwriter's perspective. She lives and breathes the mantra that 'tools don't make the artist,' applying creative modern production techniques to dated sounds and samples, breathing new life into them.
+						Combining the music of America and Japan alike, </i>alisongrace<i> explores chiptune and video game music from a songwriter's perspective. She lives and breathes the mantra that 'tools don't make the artist,' applying creative modern production techniques to old soundchips and hardware, breathing new life into them.
 					</i></p>
 					{/* <p>
 						growing up, alison's childhood was filled with music and games--both playing games and trying to make her own. her favorite part of game developemnt was writing music for them.
 					</p> */}
 					<p><i>
-						VGM composers like{' '}
+						Musicians like{' '}
+						<a href="https://anamanaguchi.com/">Anamanaguchi</a>{', '}
 						<a href="https://en.wikipedia.org/wiki/Daisuke_Amaya">Daisuke "Pixel" Amaya</a>{' '}
-						</i>(<a href="https://www.cavestory.org/game-info/about-cave-story.php">Cave Story</a>)<i>{' '}
+						</i>(<a href="https://www.cavestory.org/game-info/about-cave-story.php">Cave Story</a>)<i>{', '}
 						and{' '}
 						<a href="https://en.wikipedia.org/wiki/Toby_Fox">Toby Fox</a>{' '}
-						</i>(<a href="https://www.undertale.com/">UNDERTALE</a>)<i> are major inspirations for her--proving that she can create beautiful and evocative music with any tools.
+						</i>(<a href="https://www.undertale.com/">UNDERTALE</a>)<i> are major inspirations for her--proving that she can create beautiful, powerful, and evocative music with any tools.
 					</i></p>
 				</TextBox>
 				<div>
@@ -34,9 +35,9 @@ export default function About() {
 						className={styles.alisonCollage}
 						src="https://f4.bcbits.com/img/0010850854_21.jpg"
 					/> */}
-					<TextBox big>
+					<TextBox big subtle>
 						<p><i>
-							alisongrace uses a range of DAWs, chiptune trackers, a vast library of digital plugins, a collection of live instruments, and the support of her cat to compose and produce her music.
+							</i>alisongrace<i> uses a range of DAWs, trackers, </i>Game Boys<i>, guitars, a vast collection of physical and digital instruments, and the support of her cat to compose and produce her music.
 						</i></p>
 						<p>
 							<b>email:</b> <a href="mailto:ali@alisongrace.music">ali@alisongrace.music</a><br/>
@@ -46,8 +47,8 @@ export default function About() {
 				</div>
 			</Columns>
 			<img
-				className={styles.kasSittingImage}
-				src="/assets/kas_sitting.png"
+				className={styles.aliSittingImage}
+				src="/assets/ali_sitting.png"
 			/>
 		</>
 	);
